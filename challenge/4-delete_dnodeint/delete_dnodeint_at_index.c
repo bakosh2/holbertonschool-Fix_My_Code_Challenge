dab0deaeb560519532d91ba7fc5f1bfb84afd339
bakosh2 <bakosh2@github.com>
2026-06-22 07:47:07 +0000

    refactor(4-delete_dnodeint): clarify variable naming in delete_dnodeint_at_index.c

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,44 +11,40 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *tmp;
-    unsigned int i = 0;
+	dlistint_t *node = *head;
+	unsigned int i = 0;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    tmp = *head;
+	/* Case 1: Deleting the head node (index 0) */
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(node);
+		return (1);
+	}
 
-    /* Case 1: Deleting the head node (index 0) */
-    if (index == 0)
-    {
-        *head = tmp->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(tmp);
-        return (1);
-    }
+	/* Traverse to the node at the specified index */
+	while (node != NULL && i < index)
+	{
+		node = node->next;
+		i++;
+	}
 
-    /* Traverse to the node at the specified index */
-    while (tmp != NULL && i < index)
-    {
-        tmp = tmp->next;
-        i++;
-    }
+	/* Case 2: Index out of range (node doesn't exist) */
+	if (node == NULL)
+		return (-1);
 
-    /* Case 2: Index out of range (node doesn't exist) */
-    if (tmp == NULL)
-        return (-1);
+	/* Case 3: Deleting a middle or tail node */
+	if (node->prev != NULL)
+		node->prev->next = node->next;
 
-    /* Case 3: Deleting a middle or tail node */
-    /* Break the connection from the previous node pointing forward */
-    if (tmp->prev != NULL)
-        tmp->prev->next = tmp->next;
+	if (node->next != NULL)
+		node->next->prev = node->prev;
 
-    /* Break the connection from the next node pointing backward */
-    if (tmp->next != NULL)
-        tmp->next->prev = tmp->prev;
-
-    free(tmp);
-    return (1);
+	free(node);
+	return (1);
 }
